6170bc9fdd2a3ca23199dbfd9bbb3ac5e7c82694
kdere <kdere@b84920db-a2a6-486a-9448-392b4b104774>
2010-04-28 16:21:23 +0000

    some more moves

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(name = 'chianti',
+    version = '0.1',
+    description = 'a Python interface to the CHIANTI atomic database for astrophysical spectroscopy',
+    author = 'Ken Dere',
+    author_email = 'kdere@gmu.edu',
+    url = 'http://chiantipy.sourceforge.net',
+    download_url = 'http://sourceforge.net/projects/chiantipy',
+    package_dir = {'chianti':''},
+    packages = ['chianti','chianti.gui_qt','chianti.gui_wx','chianti.gui_cl'],
+    classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Astronomy',
+    'Topic :: Scientific/Engineering :: Physics'
+    ]
+    )
