324cc453c911a7c6aa20e70731d13c0aa7351759
kdere <kdere@b84920db-a2a6-486a-9448-392b4b104774>
2011-04-04 18:39:59 +0000

    added data.py

---
+++ data.py
@@ -0,0 +1,30 @@
+'''module for collecting various top-level Chianti data'''
+import os
+import util
+#import constants
+#import filters
+#import mputil
+#
+if os.environ.has_key('CHIANTIPY_INTERACTIVE'):
+    chInteractive = int(os.environ['CHIANTIPY_INTERACTIVE'])
+else:
+    chInteractive = 1
+
+#if chInteractive:
+#    import pylab as pl
+#else:
+#    import matplotlib
+#    matplotlib.use('Agg')
+#    import matplotlib.pyplot as pl
+###
+xuvtop = os.environ['XUVTOP']
+#chInteractive=1
+Defaults = util.defaultsRead(verbose = chInteractive)
+Ip = util.ipRead()
+MasterList = util.masterListRead()
+AbundanceAll = util.abundanceRead(abundancename = Defaults['abundfile'])
+if chInteractive:
+    IoneqAll = util.ioneqRead(ioneqname = Defaults['ioneqfile'])
+import version
+__version__ = version.__version__
+__version_info__ = version.__version_info__
