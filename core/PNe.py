0df554246bf53871b8c4c0ede321d0624e4cd689
kdere <kdere@b84920db-a2a6-486a-9448-392b4b104774>
2011-06-27 21:03:28 +0000

    added sources.py

--- core/PNe.py
+++ core/PNe.py
@@ -1,3 +1,7 @@
 class pne:
     ''' classs top model a planetary nebula'''
-    def __init__(self):
+    def __init__(self,  source, distance, temperature, density):
+        self.Sorce = source
+        self.Distance = distance
+        self.Temperature = temperature
+        self.Density = density
---
+++ sources.py
@@ -0,0 +1,3 @@
+import chianti.util as util
+def blackStar(temperature, radius, energy):
+    bb =
