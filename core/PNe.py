84dd0eddd230a0bfd8e75d887e0d8e1be1069d5f
kdere <kdere@b84920db-a2a6-486a-9448-392b4b104774>
2011-06-27 21:16:54 +0000

    typo

--- core/PNe.py
+++ core/PNe.py
@@ -1,7 +1,8 @@
+from sources import *
 class pne:
     ''' classs top model a planetary nebula'''
     def __init__(self,  source, distance, temperature, density):
-        self.Sorce = source
+        self.Source = source
         self.Distance = distance
         self.Temperature = temperature
         self.Density = density
