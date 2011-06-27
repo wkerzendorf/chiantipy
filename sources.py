6e0bdf0396c9b8e49091e6e49468d215403efda0
kdere <kdere@b84920db-a2a6-486a-9448-392b4b104774>
2011-06-27 21:15:37 +0000

    blackStar is now a class

--- sources.py
+++ sources.py
@@ -1,3 +1,24 @@
-import chianti.util as util
-def blackStar(temperature, radius, energy):
-    bb =
+class blackStar:
+    '''temperature in K, radius is the stellar radius in cm '''
+    def __init__(self, temperature, radius):
+        self.temperature = temperature
+        self.radius = radius
+    def incident(self, distance, energy):
+        ''' distance in cm and energy in erg'''
+        bb = pi*(self.radius/distance)**2*blackbody(temperature, energy)
+        self.Incident = bb
+    #
+    # ---------------------------------------------------------------------
+    #
+def blackbody(temperature, variable, hnu=1):
+    ''' to calculate the black body photon distribution as a function of energy in erg (hnu = 1) or as a function
+    of wavelength (Angstroms) (hnu=0)
+    photons cm^-2 s^-1 str^-1 ergs^-1'''
+    if hnu:
+        energy = variable
+        bb =(2./(const.planck*(const.hc**2)))*energy**2/(np.exp(energy/(const.boltzmann*temperature)) - 1.)
+        return {'photons':bb, 'temperature':temperature, 'energy':energy}
+    else:
+        wvl = 1.e-8*variable
+        bb = ((2.*const.pi*const.light)/wvl**4)/(np.exp(const.hc/(wvl*const.boltzmann*temperature)) - 1.)
+        return {'photons':bb, 'temperature':temperature, 'wvl':wvl}
--- util.py
+++ util.py
@@ -209,21 +209,6 @@ def photoxRead(ions):
         ref = lines[4:-1]
         return {'energy':energy, 'cross':cross,  'ref':ref}
     #
-    # ---------------------------------------------------------------------
-    #
-def blackbody(temperature, variable, hnu=1):
-    ''' to calculate the black body photon distribution as a function of energy in erg (hnu = 1) or as a function
-    of wavelength (Angstroms) (hnu=0)
-    photons cm^-2 s^-1 str^-1 ergs^-1'''
-    if hnu:
-        energy = variable
-        bb =(2./(const.planck*(const.hc**2)))*energy**2/(np.exp(energy/(const.boltzmann*temperature)) - 1.)
-        return {'photons':bb, 'temperature':temperature, 'energy':energy}
-    else:
-        wvl = 1.e-8*variable
-        bb = ((2.*const.pi*const.light)/wvl**4)/(np.exp(const.hc/(wvl*const.boltzmann*temperature)) - 1.)
-        return {'photons':bb, 'temperature':temperature, 'wvl':wvl}
-    #
     # -------------------------------------------------------------------------------------
     #
 def ionrecdatRead(filename):
