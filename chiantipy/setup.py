81b0f1b3516c09a833d6f6a4150be6c3210afcd9
kdere <kdere@b84920db-a2a6-486a-9448-392b4b104774>
2011-10-24 14:52:53 +0000

    stuff

--- chiantipy/setup.py
+++ chiantipy/setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 import version
 setup(name = 'ChiantiPy',
-    version = version.__version__,
     description = 'a Python interface to the CHIANTI atomic database for astrophysical spectroscopy',
     long_description = open('README').read(),
+    version = '0.4.1',
     author = 'Ken Dere',
     author_email = 'kdere@gmu.edu',
     url = 'http://chiantipy.sourceforge.net',
     download_url = 'http://sourceforge.net/projects/chiantipy',
-    package_dir = {'chianti':''},
+#    package_dir = {'chianti':''},
     packages = ['chianti','chianti.core','chianti.gui_qt','chianti.gui_wx','chianti.gui_cl'],
-    py_modules = ['FortranFormat', 'chianti.constants', 'data', 'filters', 'ionrec', 'mputil', 'chianti.sources', 'util', 'version'],
+#    py_modules = ['FortranFormat', 'chianti.constants', 'data', 'filters', 'ionrec', 'mputil', 'chianti.sources', 'util', 'version'],
     classifiers = [
     'Development Status :: 3 - Alpha',
     'Environment :: Console',
